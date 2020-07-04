3eed14f7556d7c2109bbb0a836273ef16ff2ac6f
luc1dLife <luc1d_life@protonmail.com>
2020-07-04 13:29:52 -0600

    initial commit

---
+++ Profile_0.5.0.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+contract Profile {
+
+    struct User {
+       string name;
+       string subtitle;
+       string sdescription;
+       string ldescription;
+       string weburl;
+       string imghash;
+    }
+
+    mapping(address => User) public users; 
+
+  function set(string memory _name, string memory _subtitle, string memory _sdescription,string memory _ldescription, string memory _weburl, string memory _imghash) public {
+    users[msg.sender] = User(_name, _subtitle, _sdescription, _ldescription, _weburl, _imghash);
+  }
+
+  function get() public view returns (string memory, string memory, string memory, string memory, string memory, string memory) {
+    return (
+        users[msg.sender].name, 
+        users[msg.sender].subtitle, 
+        users[msg.sender].sdescription, 
+        users[msg.sender].ldescription, 
+        users[msg.sender].weburl, 
+        users[msg.sender].imghash
+    );
+}}
